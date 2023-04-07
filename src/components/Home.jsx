76bd617b64bfa014c8f3f8d50ef52254d053e338
Fabricio Magnus <fabricio.magnus92@outlook.com>
2023-04-06 22:42:02 -0300

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import { Flex, Spacer, Image, Box, Text, Button } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import { LISTA } from "../constants/paths";
+import { AMARELOPOKEMONLOGO, AZULPOKEMONLOGO } from "../constants/colors";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -8,37 +9,52 @@ export default function Home() {
     <Flex
       w={"100vw"}
       h={"100vh"}
-      bgColor={"green"}
+      //   bgColor={"green"}
       flexDir={"column"}
       alignItems={"center"}
       p={4}
+      bgImage={"/background-home.png"}
     >
       <Image w={"40vw"} src={"/pokemon-logo.png"} />
       <Flex
         w={"60vw"}
         h={"40vh"}
-        // bgColor={"red"}
+        bgColor={"#ffffff50"}
         my={4}
         justifyContent={"space-evenly"}
         flexDir={"column"}
         alignItems={"center"}
+        borderRadius={"20px"}
+        border={"4px solid #ffffff50"}
+        filter={"blur"}
       >
-        <Text color={"white"} fontSize={"2rem"} fontWeight={"bold"}>
+        <Text color={AZULPOKEMONLOGO} fontSize={"2rem"} fontWeight={"bold"}>
           Bem Vindo a nossa Pokedex!
-        </Text>
-        <Text color={"white"} fontSize={"1.8rem"} textAlign={"center"}>
-          Este projeto foi feito para demonstrar nosso carinho e prestar nossa
-          homenagem a este anime que marcou nossa infância!
         </Text>
         <Button
           fontWeight={"bold"}
-          color={"green"}
+          height={"8vh"}
+          _hover={{
+            backgroundColor: AMARELOPOKEMONLOGO,
+            color: AZULPOKEMONLOGO,
+          }}
+          color={AMARELOPOKEMONLOGO}
+          bgColor={AZULPOKEMONLOGO}
           onClick={() => {
             navigate(LISTA);
           }}
         >
           Siga para a Lista
         </Button>
+        <Text
+          color={"black"}
+          fontSize={"1.8rem"}
+          textAlign={"center"}
+          fontWeight={"bold"}
+        >
+          Este projeto foi feito para demonstrar nosso carinho e prestar nossa
+          homenagem a este anime que marcou nossa infância!
+        </Text>
       </Flex>
     </Flex>
   );
---
+++ src/constants/colors.js
@@ -0,0 +1,2 @@
+export const AZULPOKEMONLOGO = "#3d67ba";
+export const AMARELOPOKEMONLOGO = "#fac705";
